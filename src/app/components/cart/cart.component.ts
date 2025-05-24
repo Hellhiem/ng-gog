91b50d6a8675809d8034325065d8eb4e83186414
m-szczepanek <mszczepanek23@gmail.com>
2025-05-24 16:13:21 +0200

    feat: clean up cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { GameItem } from '../../types/GameItem';
 import { GameStoreService } from '../../services/game-store/game-store.service';
 import { TranslatePipe } from '@ngx-translate/core';
@@ -6,10 +6,20 @@ import { CurrencyPipe, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
 import { SeparatorComponent } from '../separator/separator.component';
 import { CartItemComponent } from '../cart-item/cart-item.component';
+import { CartIndicatorComponent } from '../cart-indicator/cart-indicator.component';
 
 @Component({
   selector: 'app-cart',
-  imports: [TranslatePipe, NgOptimizedImage, UpperCasePipe, CurrencyPipe, ButtonComponent, SeparatorComponent, CartItemComponent],
+  imports: [
+    TranslatePipe,
+    NgOptimizedImage,
+    UpperCasePipe,
+    CurrencyPipe,
+    ButtonComponent,
+    SeparatorComponent,
+    CartItemComponent,
+    CartIndicatorComponent,
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
@@ -17,11 +27,18 @@ export class CartComponent implements OnInit {
   cartGameItems: GameItem[] = [];
   totalPrice = 0;
   gameStore = inject(GameStoreService);
+  @Input() isCartExpanded = false;
 
   ngOnInit() {
     this.gameStore.getGames().subscribe(games => {
       this.cartGameItems = games;
       this.totalPrice = games.reduce((sum, game) => sum + game.price, 0);
     });
+  }
+
+  @Output() cartClick = new EventEmitter<void>();
+
+  onClick() {
+    this.cartClick.emit();
   }
 }
